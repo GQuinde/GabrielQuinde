e2a382e20d359dc50b37d246ae15bba7c5bdab53
Gabriel Quinde <gabriel.quinde@utelvt.edu.ec>
2022-11-23 15:16:31 -0500

    mejoras

--- index.php
+++ index.php
@@ -24,8 +24,10 @@ include("marco-right.php");
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy Quinde Montero Gabriel, estudiante esmeraldeño que  desde hace 3 añosestudio en la Universidad Tecnica Luis Vargas Torres, y trabaja en diferentes areas  de Tecnología de la Información entre las cuales esta Instalacion de Software, Instalacion de Redes, Instalacion de Camaras de Seguridad  entre otras; pero en la Reparaxion de componentes es en la que mejor me desempeño. </p>
-                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en el bachillerato del Sagrado Corazó me he dedicado a la programación de computadoras. </p>
+                   <p class="eys-parrafo">Hola, yo soy Quinde Montero Gabriel, estudiante esmeraldeño que  desde hace 3 añosestudio en la Universidad Tecnica Luis Vargas Torres, y trabajo en diferentes areas  de Tecnología de la Información entre las cuales esta Instalacion de Software, Instalacion de Redes, Instalacion de Camaras de Seguridad  entre otras; pero en la Reparaxion de componentes es en la que mejor me desempeño. </p>
+		  
+ <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quizas sería  por esta capacidad que tiene estas dos áreas del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en el bachillerato del Sagrado Corazó me he dedicado a la programación de computadoras. </p>
+
 <p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el 2do de bachiller fue c++ y PHP <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo el Sagrado Corazón contaba con laboratorios de computadora  una computadora personal de marca Dell.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
@@ -33,24 +35,26 @@ include("marco-right.php");
     <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
     
 
+  <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 2017, de la especialidad de Informático, apenas me gradue travaje para la empresa de  Espek Computadores  para seguir uno de los trabajo que me formaria tanto en reparación de equipos, instalacioón de camaras de seguridad como la instalación de redes en la ciudad de Esmeraldas  donde seguí la carrera de Ingeniería en Tecnogía de la Informática y Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar para preparaeme para estudiar, prestando mis servicios profesionales en diversos locales como casas de diversos clientes ; mientras trabajaba en esta última logré ingresar a la Universidad Luis Vargas Torres de Esmeraldas</p>
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
-                   <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+ <p class="eys-parrafo">Ya con experiencia en la mano decidi ingresar a la universidad, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa Especk Computadoras, pero como los ingreso no fueron suficiente entre a trabajar a tiempo medio de manera independiente en los diversos conocimientos que aquiri en dicha empresa y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente estudio.</p>
+
+<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la facultad de Ciencias e Ingenierias, de la cual curso ahora 7mo de Tecnología de la Informática y Comunicación (TIC's).</p>
+
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
 
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Basic, utilizando el computador de la espol, luego programe c++, donde realiza algunos programa que leina el entregas del puerto serial y enviaban señales a un motor. Profesionalmente comenza programando en Informix un lenguaje de programación para el sistema Unix, utilizando uan versio de foxpro para unix, luego cuando aparecio la interface gráfica programe en foxpro y seguidamente visual foro.</p> 
-                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
+                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Visual Basic, utilizando el computador del Sagrado Corazón, luego programe c++, donde realiza algunos programa que consistia en realizar una calculadora funcional.Por aficion me meti en un curso donde aprendi a programar en Html y CSS donde realice mi primeras paginas amateur que conservo archivada en una nuve.</p> 
+                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP con los conocimientos adquiridos por los docentes que imparten las distintas masterias de programación.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta adquirir cobocimientls de diferentes areas no solo de la informática sino tambien de mecánica, arquitectura entre otras, y tambien suelo informarme muchos sobre las ultimas tendencias en tecnología.</p>
        
 
                    
